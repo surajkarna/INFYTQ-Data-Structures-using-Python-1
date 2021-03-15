8de7265c32bc4485996d5c39fd9e1855122f50d2
Suraj Karna <40906236+surajkarna@users.noreply.github.com>
2021-03-15 08:53:42 +0530

    Update Exercise 1 Level 1.py

--- Exercise 1 Level 1.py
+++ Exercise 1 Level 1.py
@@ -1,4 +1,4 @@
-Q. A teacher has given a project assignment to a class of 10 students.
+'''Q. A teacher has given a project assignment to a class of 10 students.
 
 She wants to store the marks (out of 100 ) scored by each student. The marks scored are as mentioned below:
 89,78,99,76,77,67,99,98,90
@@ -7,7 +7,7 @@ Write a python program to store the marks in a list and perform the following:
 
     1. The teacher forgot to include the marks of one student. Insert 78 at index position, 8 and display the marks of all 10 students.
     2. The teacher also wants to know the marks scored by students represented by index positions, 5 and 7. Identify and display the two marks.
-
+'''
 Code:
 
 #DSA-Exer-1
