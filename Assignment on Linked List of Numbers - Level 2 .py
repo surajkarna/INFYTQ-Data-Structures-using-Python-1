edefc8435a0f20ead13298d9170ce096033a0898
Suraj Karna <40906236+surajkarna@users.noreply.github.com>
2021-03-24 11:47:09 +0530

    Create  Assignment on Linked List of Numbers - Level 2 .py

---
+++ Assignment on Linked List of Numbers - Level 2 .py
@@ -0,0 +1,124 @@
+#lex_auth_0127438760987525121618
+
+class Node:
+    def __init__(self,data):
+        self.__data=data
+        self.__next=None
+
+    def get_data(self):
+        return self.__data
+
+    def set_data(self,data):
+        self.__data=data
+
+    def get_next(self):
+        return self.__next
+
+    def set_next(self,next_node):
+        self.__next=next_node
+
+
+class LinkedList:
+    def __init__(self):
+        self.__head=None
+        self.__tail=None
+
+    def get_head(self):
+        return self.__head
+
+    def get_tail(self):
+        return self.__tail
+
+
+    def add(self,data):
+        new_node=Node(data)
+        if(self.__head is None):
+            self.__head=self.__tail=new_node
+        else:
+            self.__tail.set_next(new_node)
+            self.__tail=new_node
+
+    def insert(self,data,data_before):
+        new_node=Node(data)
+        if(data_before==None):
+            new_node.set_next(self.__head)
+            self.__head=new_node
+            if(new_node.get_next()==None):
+                self.__tail=new_node
+
+        else:
+            node_before=self.find_node(data_before)
+            if(node_before is not None):
+                new_node.set_next(node_before.get_next())
+                node_before.set_next(new_node)
+                if(new_node.get_next() is None):
+                    self.__tail=new_node
+            else:
+                print(data_before,"is not present in the Linked list")
+
+    def display(self):
+        temp=self.__head
+        while(temp is not None):
+            print(temp.get_data())
+            temp=temp.get_next()
+
+
+    def find_node(self,data):
+        temp=self.__head
+        while(temp is not None):
+            if(temp.get_data()==data):
+                return temp
+            temp=temp.get_next()
+        return None
+
+    def delete(self,data):
+        node=self.find_node(data)
+        if(node is not None):
+            if(node==self.__head):
+                if(self.__head==self.__tail):
+                    self.__tail=None
+                self.__head=node.get_next()
+            else:
+                temp=self.__head
+                while(temp is not None):
+                    if(temp.get_next()==node):
+                        temp.set_next(node.get_next())
+                        if(node==self.__tail):
+                            self.__tail=temp
+                        node.set_next(None)
+                        break
+                    temp=temp.get_next()
+        else:
+            return False
+
+    #You can use the below __str__() to print the elements of the DS object while debugging
+    def __str__(self):
+        temp=self.__head
+        msg=[]
+        while(temp is not None):
+            msg.append(str(temp.get_data()))
+            temp=temp.get_next()
+        msg=" ".join(msg)
+        msg="Linkedlist data(Head to Tail): "+ msg
+        return msg
+
+
+def remove_duplicates(duplicate_list):
+    ptr=duplicate_list.get_head()
+    while(ptr.get_next()):
+        if(ptr.get_data()==ptr.get_next().get_data()):
+            ptr=ptr.get_next()
+            duplicate_list.delete(ptr.get_data())
+        else:
+            ptr=ptr.get_next()
+    return duplicate_list
+
+#Add different values to the linked list and test your program
+duplicate_list=LinkedList()
+duplicate_list.add(30)
+duplicate_list.add(40)
+duplicate_list.add(40)
+duplicate_list.add(40)
+duplicate_list.add(40)
+
+remove_duplicates(duplicate_list)
